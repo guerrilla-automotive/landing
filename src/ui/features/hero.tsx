1ad71be2684373def32d89d5e45b77cd82e52b19
yoyobojo <angellolazar@gmail.com>
2023-03-01 16:33:01 -0500

    ready to implement content

--- src/ui/features/hero.tsx
+++ src/ui/features/hero.tsx
@@ -5,8 +5,8 @@ const Hero = () => (
   <Background color="bg-gray-200" fullscreen>
     <Section
       title="Guerrilla Automotive"
-      description="Bringing the repair shop to you"
-      yPadding="pt-20 pb-32"
+      description="Bringing the repair shop to you, no matter where, no matter for what"
+      yPadding="py-20"
     >
       <div>Content</div>
     </Section>
